7d1c346e6eb007ac07fdea1cbb8b87446e35cbda
AkhmadRafiHaidarrahman <nakanom858@gmail.com>
2025-11-08 09:08:26 +0700

    easy

---
+++ 7-Debug/debug.js
@@ -0,0 +1,14 @@
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+// Examples
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+
+
+function reverseWords(str) {
+  // Go for it
+    let rev = str.split("").reverse("").join("")
+    let ori = rev.split(" ").reverse("").join(" ")
+  return ori
+}
+console.log(reverseWords("This is an example!"))
